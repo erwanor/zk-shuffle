f9f84cb330132e9ac092719e5aab96ad376d4d9b
erwanor <erwan.ounn.84@gmail.com>
2022-06-10 15:27:29 +0200

    feat(matrix): cipher matrix scaffolding

---
+++ src/ciphers/matrix.rs
@@ -0,0 +1,25 @@
+struct CipherMatrix {
+    // const generics?
+}
+
+impl CipherMatrix {
+    fn new() -> Self {
+        unimplemented!()
+    }
+
+    fn iter(&mut self, closure: ()) {
+        unimplemented!()
+    }
+
+    fn parallel_iter(&mut self, closure: ()) {
+        unimplemented!()
+    }
+
+    fn transposed_vandermonde(&mut self) {
+        unimplemented!()
+    }
+
+    fn column_vectors(&self) -> Vec<()> {
+        unimplemented!()
+    }
+}
