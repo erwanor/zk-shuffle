d8c9dba0375487d2b2f365788209498732ce3108
erwanor <erwan.ounn.84@gmail.com>
2022-06-10 13:43:24 +0200

    feat: define minimal API

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,55 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use merlin;
+use rand_core::{CryptoRng, RngCore};
+
+// some homomorphic cipher here
+struct Cipher {}
+
+struct Proof {}
+
+struct BayerGrothShuffler<R: CryptoRng + RngCore> {
+    num_columns: usize,
+    num_rows: usize,
+    rows: Vec<Cipher>,
+    columns: Vec<Cipher>,
+    internal_shuffler: safe_shuffle::SafeShuffler<R>,
+    internal_rng: R,
+    transcript: merlin::Transcript,
+    verifier: BayerGrothVerifier,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+struct BayerGrothVerifier {}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+enum ProverError {
+    SomethingSomething,
+}
+
+impl<R> BayerGrothShuffler<R>
+where
+    R: CryptoRng + RngCore,
+{
+    fn new() -> Self {
+        unimplemented!()
+    }
+
+    fn read_input(&mut self, ciphers: Vec<Cipher>) -> Result<Self, ProverError> {
+        unimplemented!()
+    }
+
+    fn shuffle(&mut self, rounds: usize) -> Result<Proof, ProverError> {
+        unimplemented!()
+    }
+}
+
+impl BayerGrothVerifier {
+    fn new() -> Self {
+        unimplemented!()
+    }
+
+    fn verify_precompute(&self, proof: Proof) -> bool {
+        unimplemented!()
+    }
+
+    fn verify(proof: Proof) -> bool {
+        false
     }
 }
